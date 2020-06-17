c84c8c6cc940051253927badc018b699f82bab39
Snikerso <42245963+Snikerso@users.noreply.github.com>
2020-06-17 11:22:40 +0200

    tensorflow add

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import 
+from decouple import config
 from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
